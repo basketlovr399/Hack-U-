f0de349e94e15ddf7327599ded8212fe673ed00d
Koyo Yamamoto <58037205+basketlovr399@users.noreply.github.com>
2022-02-22 15:11:39 +0900

    Update Cal_interface.java

--- Cal_interface.java
+++ Cal_interface.java
@@ -1,9 +1,19 @@
-interface Cal_interface {
-  
+abstract class Calories {
+  int total = 0;
 //   カロリーの合計
-  int sum_calories();
+  int sum_calories(int array[]){
+    for (int i = 0; i <= array.length; i++){
+        total += array[i];
+    }
+    return total;
+  } 
   
 //   出力
-  void show_food();
+  void show_food(String array[]){
+    for (i = 0; i <= array.length; i++){
+      System.out.print(array[i]);
+    }
+  }
+  
   void show_calories();
 }
